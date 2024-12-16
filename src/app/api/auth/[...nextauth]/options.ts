cbd7b75feec561954f6e01cb56f5f7ff059f02d9
VyasVishal-IO <vyasvishal.dev@gmail.com>
2024-12-16 16:26:11 +0530

    Update options.ts

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -66,7 +66,7 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: 'jwt',
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: "vyasvishal",
   pages: {
     signIn: '/sign-in',
   },
