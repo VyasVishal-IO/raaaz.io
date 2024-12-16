1b9b43fc2a4bab0f1b64ec6dfe64efc64f0075c4
VyasVishal-IO <vyasvishal.dev@gmail.com>
2024-12-16 19:24:03 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from '@/components/ui/toaster';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'SecureEcho',
+  title: 'Raaaz.io',
   description: 'Real feedback from real people.',
 };
 
